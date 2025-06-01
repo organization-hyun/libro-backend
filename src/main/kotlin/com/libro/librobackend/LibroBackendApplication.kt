c857821098ec7b08cd0fbea05d4ab7c3a79ff654
hyunjuuun <hyunjun86120@gmail.com>
2025-06-01 13:36:03 +0900

    feat: 환경 변수 처리

--- src/main/kotlin/com/libro/librobackend/LibroBackendApplication.kt
+++ src/main/kotlin/com/libro/librobackend/LibroBackendApplication.kt
@@ -1,9 +1,11 @@
 package com.libro.librobackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class LibroBackendApplication
 
 fun main(args: Array<String>) {
